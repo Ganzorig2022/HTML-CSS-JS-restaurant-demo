5392aa5454636b5cbbb22a5148d795b2e84f920c
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-05-30 23:34:04 +0800

    profile updated

--- food/profile.js
+++ food/profile.js
@@ -1,3 +1,31 @@
+// =================Button Effect====================
+const buttons = document.querySelectorAll(".ripple");
+console.log(buttons);
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    // mouse-iin bairshliig X, Y toogoor gargaj ogno.
+    const x = e.clientX;
+    const y = e.clientY;
+
+    // button bairshliig gargaj ogno.
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    const circle = document.createElement("span");
+
+    // circle.className = "circle"- ni classList.add-tai adilhan.
+    circle.classList.add("circle");
+    this.appendChild(circle);
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    setTimeout(() => circle.remove(), 200);
+  });
+});
+// ========================Profile window Open=====================
 const userProfileBtn = document.getElementsByClassName("profile")[0];
 
 userProfileBtn.addEventListener("click", () => {
@@ -5,3 +33,30 @@ userProfileBtn.addEventListener("click", () => {
   const userProfile = document.getElementsByClassName("user-profile-modal")[0];
   userProfile.classList.toggle("hidden");
 });
+
+// ========================Profile Menu Show Hide==================
+
+const content1 = document.getElementsByClassName("content1")[0];
+const content2 = document.getElementsByClassName("content2")[0];
+const listItems = document.querySelectorAll(".left-content li");
+
+listItems.forEach((item, idx) => {
+  item.addEventListener("click", () => {
+    hideAllListItems();
+    item.classList.add("active");
+    if (idx === 1) {
+      content2.classList.add("show");
+    } else {
+      content2.classList.remove("show");
+    }
+    if (idx === 0) {
+      content1.classList.add("show");
+    } else {
+      content1.classList.remove("show");
+    }
+  });
+});
+
+function hideAllListItems() {
+  listItems.forEach((content) => content.classList.remove("active"));
+}
