05fcb9048db55586e0f6649e6a41e84a20eb34ea
Ganzorig <103576449+Ganzorig2022@users.noreply.github.com>
2022-06-09 12:10:29 +0800

    Branch ganzo (#6)
    * home.html dr SIGNUP, LOGIN modal hiij javascript, firebase holboj ogow.
    
    * firebase updatelaw.
    
    * firebase js-dr function zasaw
    
    * profile dr firestore-ig nemew.
    
    * profile dr zahialganii hesgiig yanzlaw.
    
    * home.js, auth.js dr input-uud hooson bhad submit darahad uuseh BUG-iig zasaw
    
    * user profile newterher ongo ni soligdono.
    
    * profile js dr function-uud zasaw.
    
    * home, profile page dr FOOTER nemew.
    
    * removed node modules

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+import { signUp, signIn, logOut } from "./firebase_auth.js";
+
 // Run getRatings when DOM loads
 document.addEventListener("DOMContentLoaded", getRatings);
 
@@ -16,7 +18,25 @@ function getRatings() {
   // Set width of stars-inner to percentage
   const starsInner = document.querySelector(".stars-inner");
   starsInner.style.width = starPercentageRounded;
+  showUserName();
 }
+// ==================Scroll To Top Effect===============
+const scrollToTopBtn = document.getElementById("scrollToTop-button");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
 // =================Button Effect====================
 const buttons = document.querySelectorAll(".ripple");
@@ -47,16 +67,230 @@ buttons.forEach((button) => {
 
 // ========================Profile window Open=====================
 const userProfileBtn = document.getElementsByClassName("profile")[0];
+const userProfile = document.getElementsByClassName("user-profile-modal")[0];
 
 userProfileBtn.addEventListener("click", () => {
-  const userProfile = document.getElementsByClassName("user-profile-modal")[0];
   userProfile.classList.toggle("hidden");
 });
 
 // ========================Cart Modal Window Open=====================
 const cartIconBtn = document.getElementsByClassName("fa-shopping-cart")[0];
+const cartModal = document.getElementsByClassName("cart-modal")[0];
 
 cartIconBtn.addEventListener("click", () => {
-  const cartModal = document.getElementsByClassName("cart-modal")[0];
   cartModal.classList.toggle("hidden");
 });
+
+// ========================LOGIN MODAL window Open=====================
+const loginOpen = document.getElementById("login-open");
+const loginModal = document.getElementsByClassName("login-modal-container")[0];
+const loginClose = document.getElementById("modal-close-btn");
+
+loginOpen.addEventListener("click", () => {
+  loginModal.classList.add("show-modal");
+  clearLoginInputs();
+});
+loginClose.addEventListener("click", () => {
+  loginModal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == loginModal) {
+    loginModal.classList.remove("show-modal");
+  }
+});
+
+// ========================SIGNUP MODAL window Open=====================
+let signupOpen = document.getElementById("signup-open");
+let signupModal = document.getElementById("signup-modal");
+const signupClose = document.getElementById("signup-close-btn");
+
+signupOpen.addEventListener("click", () => {
+  signupModal.classList.add("show-modal");
+  clearSignupInputs();
+});
+signupClose.addEventListener("click", () => {
+  signupModal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", (e) => {
+  e.target == signupModal ? signupModal.classList.remove("show-modal") : false;
+});
+
+//////////////////////////////////FIRESTORE///////////////////////////
+// =============================GLOBAL VARIABLES=======================
+const signupBtn = document.getElementById("signup-submit-btn");
+const signinBtn = document.getElementById("login-submit-btn");
+const logoutBtn = document.getElementById("logout-btn");
+const userProfileModal =
+  document.getElementsByClassName("user-profile-modal")[0];
+const userProfileModalHeader = document.getElementById(
+  "user-profile-modal--header"
+);
+let loginInputs = document.querySelectorAll(".login-modal-form input");
+
+let isSuccessful = false;
+
+// ===========================SIGN UP NEW USER=======================
+signupBtn.addEventListener("click", async () => {
+  const signupName = document.getElementById("signup-name").value;
+  const signupEmail = document.getElementById("signup-email").value;
+  const signupPassword = document.getElementById("signup-password").value;
+  const signupPassword2 = document.getElementById("signup-password2").value;
+
+  if (
+    checkRequiredInputs([
+      signupName,
+      signupEmail,
+      signupPassword,
+      signupPassword2,
+    ])
+  ) {
+    isSuccessful = await signUp(signupEmail, signupPassword, signupName);
+
+    if (isSuccessful) {
+      swal("Шинэ хэрэглэгч амжилттай үүслээ!");
+
+      signupModal.classList.remove("show-modal");
+
+      disableLoginInputs();
+      clearSignupInputs();
+      disableSignUpBtn();
+      disableLoginBtn();
+      activeUserProfile();
+    } else {
+      swal("Шинэ хэрэглэгч үүсэхэд алдаа гарлаа!");
+
+      enableLoginInputs();
+      enableSignUpBtn();
+    }
+  } else {
+    swal("Таны бөглөх талбарууд хоосон байна!");
+  }
+});
+
+// ===========================SIGN IN EXISTING USER=======================
+signinBtn.addEventListener("click", async () => {
+  const signinEmail = document.getElementById("login-email").value;
+  const signinPassword = document.getElementById("login-password").value;
+
+  if (checkRequiredInputs([signinEmail, signinPassword])) {
+    isSuccessful = await signIn(signinEmail, signinPassword);
+
+    if (isSuccessful) {
+      swal("Та амжилттай нэвтэрлээ!");
+
+      loginModal.classList.remove("show-modal");
+      clearLoginInputs();
+      disableLoginInputs();
+      disableLoginBtn();
+      disableSignUpBtn();
+      activeUserProfile();
+
+      showUserName();
+    } else {
+      swal("Нэвтрэлт амжилтгүй. Хэрэглэгч олдсонгүй!");
+    }
+  } else {
+    swal("Таны бөглөх талбарууд хоосон байна!");
+  }
+});
+
+// =========================LOG OUT USER=================
+logoutBtn.addEventListener("click", async () => {
+  isSuccessful = await logOut();
+  if (isSuccessful) {
+    swal("Та системээс гарлаа.");
+    // localStorage-iig empty bolgono.
+    localStorage.removeItem("loggedUserUid");
+
+    userProfileModal.classList.remove("hidden");
+    userProfileModalHeader.innerHTML = `Хэрэглэгч:`;
+
+    enableLoginInputs();
+    enableLoginBtn();
+    enableSignUpBtn();
+    clearLoginInputs();
+    inActiveUserProfile();
+  } else {
+    disableLoginInputs();
+  }
+});
+
+// INPUT-uud hooson esehiig shalgadag function()
+function checkRequiredInputs(inputArr) {
+  return !inputArr.includes("");
+}
+// SIGN IN hiisnii daraa input-uudiig IDEWHGV bolgoh
+function disableLoginInputs() {
+  loginInputs.forEach((input) => {
+    input.setAttribute("disabled", "");
+  });
+}
+
+// SIGN OUT hiisnii daraa input-uudiig IDEWHTEI bolgoh
+function enableLoginInputs() {
+  loginInputs.forEach((input) => {
+    input.removeAttribute("disabled");
+  });
+}
+// SIGN IN hiisnii daraa input-uudiig CLEAR hiih
+function clearLoginInputs() {
+  const signinEmail = document.getElementById("login-email");
+  const signinPassword = document.getElementById("login-password");
+
+  signinEmail.value = "";
+  signinPassword.value = "";
+}
+
+// SIGN OUT hiisnii daraa input-uudiig CLEAR hiih
+function clearSignupInputs() {
+  const signupName = document.getElementById("signup-name");
+  const signupEmail = document.getElementById("signup-email");
+  const signupPassword = document.getElementById("signup-password");
+
+  signupName.value = "";
+  signupEmail.value = "";
+  signupPassword.value = "";
+}
+
+// SIGN IN hiisnii daraa LOGIN button-iig NONE bolgoh
+function disableLoginBtn() {
+  loginOpen.style.display = "none";
+}
+// SIGN OUT hiisnii daraa LOGIN button-iig IDEWHTEI bolgoh
+function enableLoginBtn() {
+  loginOpen.style.display = "block";
+}
+// SIGN UP hiisnii daraa LOGIN button-iig NONE bolgoh
+function disableSignUpBtn() {
+  signupOpen.style.display = "none";
+}
+
+// SIGN UP hiisnii daraa LOGIN button-iig IDEWHTEI bolgoh
+function enableSignUpBtn() {
+  signupOpen.style.display = "block";
+}
+function activeUserProfile() {
+  const userIcon = document.getElementsByClassName("fa-user")[0];
+  userIcon.style.color = "#fff";
+  userProfileBtn.style.background = "#fb1c25";
+}
+function inActiveUserProfile() {
+  const userIcon = document.getElementsByClassName("fa-user")[0];
+  userIcon.style.color = "#000";
+  userProfileBtn.style.background = "#ccc";
+}
+
+// =========================USER NAME-iig Haruulah=================
+function showUserName() {
+  if (localStorage.length > 0) {
+    //Items are stored in local storage
+    let userData = JSON.parse(localStorage.getItem("loggedUserUid"));
+    let userName = userData.name;
+
+    userProfileModalHeader.innerHTML = `Хэрэглэгч: ${userName}`;
+  } else {
+    console.log("Local storage is empty");
+  }
+}
--- profile.js
+++ profile.js
@@ -1,6 +1,33 @@
+import {
+  signUp,
+  signIn,
+  logOut,
+  updateUserDataInFireStore,
+} from "./firebase_auth.js";
+
+// DOM Refresh hiigdehed ehleed ajillana.
+document.addEventListener("DOMContentLoaded", showUserName);
+
+// ==================Scroll To Top Effect===============
+const scrollToTopBtn = document.getElementById("scrollToTop-button");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
 // =================Button Effect====================
 const buttons = document.querySelectorAll(".ripple");
-console.log(buttons);
 buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
     // mouse-iin bairshliig X, Y toogoor gargaj ogno.
@@ -25,14 +52,6 @@ buttons.forEach((button) => {
     setTimeout(() => circle.remove(), 200);
   });
 });
-// ========================Profile window Open=====================
-const userProfileBtn = document.getElementsByClassName("profile")[0];
-
-userProfileBtn.addEventListener("click", () => {
-  console.log("clicked");
-  const userProfile = document.getElementsByClassName("user-profile-modal")[0];
-  userProfile.classList.toggle("hidden");
-});
 
 // ========================Profile Menu Show Hide==================
 
@@ -45,11 +64,13 @@ listItems.forEach((item, idx) => {
     hideAllListItems();
     item.classList.add("active");
     if (idx === 1) {
+      //hereglegchiin zahialga tsonh
       content2.classList.add("show");
     } else {
       content2.classList.remove("show");
     }
     if (idx === 0) {
+      //huwiin medeelel tsonh
       content1.classList.add("show");
     } else {
       content1.classList.remove("show");
@@ -68,3 +89,272 @@ cartIconBtn.addEventListener("click", () => {
   const cartModal = document.getElementsByClassName("cart-modal")[0];
   cartModal.classList.toggle("hidden");
 });
+
+// ========================LOGIN MODAL window Open=====================
+const loginOpen = document.getElementById("login-open");
+const loginModal = document.getElementById("login-modal");
+const loginClose = document.getElementById("modal-close-btn");
+
+loginOpen.addEventListener("click", () => {
+  loginModal.classList.add("show-modal");
+  clearLoginInputs();
+});
+
+loginClose.addEventListener("click", () => {
+  loginModal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", (e) => {
+  e.target == loginModal ? loginModal.classList.remove("show-modal") : false;
+});
+
+// ========================SIGNUP MODAL window Open=====================
+let signupOpen = document.getElementById("signup-open");
+let signupModal = document.getElementById("signup-modal");
+const signupClose = document.getElementById("signup-close-btn");
+
+signupOpen.addEventListener("click", () => {
+  signupModal.classList.add("show-modal");
+  clearSignupInputs();
+});
+signupClose.addEventListener("click", () => {
+  signupModal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", (e) => {
+  e.target == signupModal ? signupModal.classList.remove("show-modal") : false;
+});
+
+//////////////////////////////////FIRESTORE///////////////////////////
+// =============================GLOBAL VARIABLES=======================
+const signupBtn = document.getElementById("signup-submit-btn");
+const signinBtn = document.getElementById("login-submit-btn");
+const logoutBtn = document.getElementById("user-logout-btn");
+const userProfileBtn = document.getElementsByClassName("profile")[0];
+let loginInputs = document.querySelectorAll(".login-modal-form input");
+const loggedUserId = document.getElementById("logged-user-id");
+
+const userIcon = document.getElementsByClassName("fa-user")[0];
+
+let isSuccessful = false;
+
+// ===========================SIGN UP NEW USER=======================
+signupBtn.addEventListener("click", async () => {
+  const signupName = document.getElementById("signup-name").value;
+  const signupEmail = document.getElementById("signup-email").value;
+  const signupPassword = document.getElementById("signup-password").value;
+  const signupPassword2 = document.getElementById("signup-password2").value;
+
+  if (
+    checkRequiredInputs([
+      signupName,
+      signupEmail,
+      signupPassword,
+      signupPassword2,
+    ])
+  ) {
+    isSuccessful = await signUp(signupEmail, signupPassword, signupName);
+
+    if (isSuccessful) {
+      swal("Шинэ хэрэглэгч амжилттай үүслээ!");
+
+      signupModal.classList.remove("show-modal");
+
+      disableLoginInputs();
+      clearSignupInputs();
+      disableSignUpBtn();
+      disableLoginBtn();
+    } else {
+      swal("Шинэ хэрэглэгч үүсэхэд алдаа гарлаа!");
+
+      enableLoginInputs();
+      enableSignUpBtn();
+    }
+  } else {
+    swal("Таны бөглөх талбарууд хоосон байна!");
+  }
+});
+
+// INPUT-uud hooson esehiig shalgadag function()
+function checkRequiredInputs(inputArr) {
+  return !inputArr.includes("");
+}
+
+// ===========================SIGN IN EXISTING USER=======================
+signinBtn.addEventListener("click", async () => {
+  const signinEmail = document.getElementById("login-email").value;
+  const signinPassword = document.getElementById("login-password").value;
+
+  if (checkRequiredInputs([signinEmail, signinPassword])) {
+    isSuccessful = await signIn(signinEmail, signinPassword);
+
+    if (isSuccessful) {
+      swal(`Хэрэглэгч "${showUserName()}" та амжилттай нэвтэрлээ!`);
+
+      loginModal.classList.remove("show-modal");
+      clearLoginInputs();
+      disableLoginBtn();
+      disableSignUpBtn();
+      activeUserProfile();
+
+      showUserName();
+      content1.classList.add("show");
+
+      showUserInfo();
+    } else {
+      swal("Нэвтрэлт амжилтгүй. Хэрэглэгч олдсонгүй!");
+      clearLoginInputs();
+    }
+  } else {
+    swal("Таны бөглөх талбарууд хоосон байна!");
+  }
+});
+
+// =========================LOG OUT USER=================
+logoutBtn.addEventListener("click", async () => {
+  isSuccessful = await logOut();
+  if (isSuccessful) {
+    swal("Та системээс гарлаа. Дахин нэвтэрч орно уу.");
+    localStorage.clear("loggedUserUid");
+    content1.classList.add("show");
+    loggedUserId.textContent = "байхгүй!";
+
+    enableLoginBtn();
+    enableSignUpBtn();
+    clearLoginInputs();
+    inActiveUserProfile();
+    clearUserInputs();
+  } else {
+    disableLoginBtn();
+  }
+});
+
+//======================= SIGN IN hiisnii daraa hereglegchiin medeelliig HARUULAH
+function showUserInfo() {
+  if (localStorage.length > 0) {
+    //Items are stored in local storage
+    let userData = JSON.parse(localStorage.getItem("loggedUserUid"));
+    let FirstName = userData.firstname;
+    let LastName = userData.lastname;
+    let Password = userData.password;
+    let Email = userData.email;
+
+    userFirstName.value = FirstName;
+    userLastName.value = LastName;
+    userLoginPassword.value = Password;
+    userLoginEmail.value = Email;
+  } else {
+    console.log("Local storage is empty");
+  }
+}
+
+// =========================USER NAME-iig Haruulah=================
+function showUserName() {
+  if (localStorage.length > 0) {
+    //Items are stored in local storage
+    let userData = JSON.parse(localStorage.getItem("loggedUserUid"));
+    let userName = userData.name;
+    // console.log("Username: ", userName);
+
+    loggedUserId.textContent = userName;
+    return userName;
+  } else {
+    console.log("Local storage is empty");
+  }
+}
+
+// =============Update User Info to Firestore=============
+const saveUserInfoBtn = document.getElementById("save-user-info-btn");
+
+saveUserInfoBtn.addEventListener("click", async () => {
+  const userFirstName = document.getElementById("user-first-name").value;
+  const userLastName = document.getElementById("user-last-name").value;
+  const userLoginPassword = document.getElementById(
+    "user-login-password"
+  ).value;
+  const userLoginEmail = document.getElementById("user-login-email").value;
+
+  if (
+    checkRequiredInputs([
+      userFirstName,
+      userLastName,
+      userLoginPassword,
+      userLoginEmail,
+    ])
+  ) {
+    isSuccessful = await updateUserDataInFireStore(
+      userFirstName,
+      userLastName,
+      userLoginPassword,
+      userLoginEmail
+    );
+    if (isSuccessful) {
+      swal("Мэдээлэл хадгалагдлаа");
+    } else {
+      swal("Амжилтгүй!");
+    }
+  } else {
+    swal("Таны бөглөх талбарууд хоосон байна!");
+  }
+});
+
+// SIGN IN hiisnii daraa input-uudiig CLEAR hiih
+function clearLoginInputs() {
+  const signinEmail = document.getElementById("login-email");
+  const signinPassword = document.getElementById("login-password");
+
+  signinEmail.value = "";
+  signinPassword.value = "";
+}
+
+// SIGN OUT hiisnii daraa input-uudiig CLEAR hiih
+function clearSignupInputs() {
+  const signupName = document.getElementById("signup-name");
+  const signupEmail = document.getElementById("signup-email");
+  const signupPassword = document.getElementById("signup-password");
+
+  signupName.value = "";
+  signupEmail.value = "";
+  signupPassword.value = "";
+}
+// SIGN OUT hiisnii daraa hereglegchiin medeelliig CLEAR hiih
+const userFirstName = document.getElementById("user-first-name");
+const userLastName = document.getElementById("user-last-name");
+const userLoginPassword = document.getElementById("user-login-password");
+const userLoginEmail = document.getElementById("user-login-email");
+
+function clearUserInputs() {
+  userFirstName.value = "";
+  userLastName.value = "";
+  userLoginPassword.value = "";
+  userLoginEmail.value = "";
+}
+
+// SIGN IN hiisnii daraa LOGIN button-iig IDEWHGVI bolgoh
+function disableLoginBtn() {
+  loginOpen.style.display = "none";
+}
+
+// SIGN OUT hiisnii daraa LOGIN button-iig IDEWHTEI bolgoh
+function enableLoginBtn() {
+  loginOpen.style.display = "block";
+}
+
+// SIGN UP hiisnii daraa LOGIN button-iig IDEWHGVI bolgoh
+function disableSignUpBtn() {
+  signupOpen.style.display = "none";
+}
+
+// SIGN UP hiisnii daraa LOGIN button-iig IDEWHTEI bolgoh
+function enableSignUpBtn() {
+  signupOpen.style.display = "block";
+}
+
+function activeUserProfile() {
+  userIcon.style.color = "#fff";
+  userProfileBtn.style.background = "#fb1c25";
+}
+function inActiveUserProfile() {
+  userIcon.style.color = "#000";
+  userProfileBtn.style.background = "#ccc";
+}
